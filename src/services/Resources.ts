539a75bfc59f8b3b6a55d680b51b06b9529bd9f4
Exibit <dmitriy.maznyak@ideus.biz>
2023-07-26 21:13:20 +0300

    PJA-3 [Front-End] -added vercel.json

--- src/services/Resources.ts
+++ src/services/Resources.ts
@@ -1,11 +1,11 @@
 export const ApiResources = {
-  apiUrl: 'https://projects-app-backend.vercel.app/api',
+  apiUrl: "https://projects-app-backend.vercel.app/api",
 };
 
 export const Resources = {
-  signIn: 'sign-in',
-  signUp: 'sign-up',
-  profile: 'profile',
+  signIn: "sign-in",
+  signUp: "sign-up",
+  profile: "profile",
   apiSignIn: `${ApiResources}/sign-in`,
   apiSignUp: `${ApiResources}/sign-up`,
 };
